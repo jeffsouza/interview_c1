18bbceb8da0d39c52a310342f389264bbc796ba7
Jefferson <jeffsouza@gmail.com>
2023-04-20 12:56:22 -0300

    Implementing with recursivity

--- interview_c1/src/main/java/org/example/Main.java
+++ interview_c1/src/main/java/org/example/Main.java
@@ -17,7 +17,24 @@ public class Main {
 
     }
 
+    public static void operation2(int value) {
+
+        if (value == 1) {
+            return;
+        }
+
+        if (value % 2 == 0) {
+            value /= 2;
+        } else {
+            value = value * 3 + 1;
+        }
+        System.out.println(value);
+
+        operation2(value);
+
+    }
+
     public static void main(String[] args) {
-        operation(50);
+        operation2(50);
     }
 }
