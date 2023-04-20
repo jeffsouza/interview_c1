0d86fb27fde3cd27c0704ec33e5f64ffc3ca58ef
Jefferson <jeffsouza@gmail.com>
2023-04-20 12:52:43 -0300

    Function operation with the expected results

--- interview_c1/src/main/java/org/example/Main.java
+++ interview_c1/src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
 public class Main {
+
+    public static void operation(int value) {
+
+        while (value > 1) {
+
+            if (value % 2 == 0) {
+                value /= 2;
+            } else {
+                value = value * 3 + 1;
+            }
+
+            System.out.println(value);
+        }
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        operation(50);
     }
 }
